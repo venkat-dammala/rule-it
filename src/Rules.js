c6e88291571a1307ac0fdb28497147010f593d43
Dammala <venkat77767@gmail.com>
2018-01-19 17:42:48 -0500

    first commit

--- src/Rules.js
+++ src/Rules.js
@@ -1,13 +1,27 @@
 import React,  { Component } from 'react';
-
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
 export default class Rules extends Component {
     render() {
         return (
-          <div>
-            <p>
-              Start editing toc happen :)
-            </p>
-          </div>
+          <Form>
+            <h3>Add Rules</h3>
+            <div>
+                <label>
+                    Rule Id
+                    <Input type = "text" name='ruleId' validations={[required]}/>
+                </label>
+            </div>
+            <div>
+                <label>
+                    Description
+                    <Input type='text' name='desc' validations={[required]}/>
+                </label>
+            </div>
+            <div>
+                <Button>Add</Button>
+            </div>
+        </Form>
         );
       }
 }
